ce391972813bec91ee98d1b05bbd5ca139de5919
Axel26767 <122137987+Axel26767@users.noreply.github.com>
2023-10-06 06:38:50 -0600

    Add files via upload

---
+++ web/index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".datnews-toggle")
+const navDatanews = document.querySelector(".datanews")
+
+navToggle,addEventListener("click", () =>{
+    navDatanews.classList.toggle("datanews_visible")
+})
